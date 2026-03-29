b56487e077af7e339ca2c71b827c50fe028d0780
miltonyagual75-spec <miltonyagual75@gmail.com>
2026-03-29 10:50:54 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+
+const CACHE_NAME = "ueser-pro-cache-v1";
+const URLS_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+self.addEventListener('install', e=>{
+ e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(URLS_TO_CACHE)));
+});
+self.addEventListener('fetch', e=>{
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
